13d8d5334eff949edabe6e2affc902cbfd899ad4
robin0172 <61098934+robin0172@users.noreply.github.com>
2020-10-08 08:07:44 +0600

    Updates

---
+++ precache-manifest.7769485e7c3b79c180d5295575f82aa0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8888c73af86f1d09d60a26d5493094",
+    "url": "/rectjsnew/index.html"
+  },
+  {
+    "revision": "e12800e17944f1e44c1f",
+    "url": "/rectjsnew/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "5f89238ea13ef94cf8e5",
+    "url": "/rectjsnew/static/css/main.bd1aa676.chunk.css"
+  },
+  {
+    "revision": "e12800e17944f1e44c1f",
+    "url": "/rectjsnew/static/js/2.2aa98915.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/rectjsnew/static/js/2.2aa98915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f89238ea13ef94cf8e5",
+    "url": "/rectjsnew/static/js/main.724e39f0.chunk.js"
+  },
+  {
+    "revision": "474c0dbd42a46c71917d",
+    "url": "/rectjsnew/static/js/runtime-main.ee38af75.js"
+  },
+  {
+    "revision": "3b76a2a2df16f13b30c1147f25c4acb8",
+    "url": "/rectjsnew/static/media/About.3b76a2a2.jpg"
+  },
+  {
+    "revision": "a76f7221f658b9655e86d3d50e69dbb4",
+    "url": "/rectjsnew/static/media/Python-011.a76f7221.png"
+  },
+  {
+    "revision": "c91b37d84ebca413e79510f3708b80ee",
+    "url": "/rectjsnew/static/media/html.c91b37d8.png"
+  },
+  {
+    "revision": "a52d42e3724a9f2cb0b3c0db7ae209d0",
+    "url": "/rectjsnew/static/media/javascript.a52d42e3.jpg"
+  },
+  {
+    "revision": "c9f9927dd6efa036ad38f88beb4f1c0c",
+    "url": "/rectjsnew/static/media/man-with-start-up-rocket-smartphone-website-vector-design_24877-65595.c9f9927d.jpg"
+  },
+  {
+    "revision": "f421c0755ed35d098f49f0665de0ca2c",
+    "url": "/rectjsnew/static/media/s1.f421c075.jpg"
+  },
+  {
+    "revision": "36787df9893681ad10fcd5bf63d67e79",
+    "url": "/rectjsnew/static/media/seo.36787df9.jpg"
+  },
+  {
+    "revision": "8bb0d4dc0fee9983b176262628c8e60b",
+    "url": "/rectjsnew/static/media/webdevelopment.8bb0d4dc.jpg"
+  }
+]);
